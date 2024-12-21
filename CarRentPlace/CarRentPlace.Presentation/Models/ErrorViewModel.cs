0cd1bdf017d001ca0ea37a2c6caa4a7f504951f8
Andrey Kiseliov <andrey.kiseliov@modsen-software.com>
2024-12-21 18:13:36 +0300

    Created skeleton

---
+++ CarRentPlace/CarRentPlace.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarRentPlace.Presentation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
