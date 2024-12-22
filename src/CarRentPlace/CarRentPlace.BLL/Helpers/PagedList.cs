5e7e2c870bc02236daa99d844a84d8d5002c9798
Andrey Kiseliov <andrey.kiseliov@modsen-software.com>
2024-12-22 15:56:14 +0300

    Added paged list

---
+++ src/CarRentPlace/CarRentPlace.BLL/Helpers/PagedList.cs
@@ -0,0 +1,30 @@
+namespace CarRentPlace.BLL.Helpers;
+
+public class PagedList<T> : List<T>
+{
+    public int CurrentPage { get; private set; }
+    public int TotalPages { get; private set; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
+
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+
+    public PagedList() : base()
+    {
+        TotalCount = 0;
+        PageSize = 0;
+        CurrentPage = 0;
+        TotalPages = 0;
+    }
+    
+    public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageSize = pageSize;
+        CurrentPage = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+        AddRange(items);
+    }
+}
